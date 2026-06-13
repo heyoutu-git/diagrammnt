fcfe5953cfac0d96d9efad9a23ce3a3c0fa91c29
heyoutu-git <19005227@qq.com>
2026-06-13 11:30:56 +0800

    Add DiagrammntApplication.java

---
+++ backend/src/main/java/com/diagrammnt/DiagrammntApplication.java
@@ -0,0 +1,19 @@
+package com.diagrammnt;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@MapperScan("com.diagrammnt.mapper")
+@ServletComponentScan
+@EnableAsync
+@EnableScheduling
+public class DiagrammntApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiagrammntApplication.class, args);
+    }
+}
